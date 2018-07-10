064f9a7516c1240880cec83e3f2b41d999559593
juanvallejo <jvallejo@redhat.com>
2018-07-09 22:26:48 -0400

    add new method to App

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,22 @@ pub struct App {
 }
 
 impl App {
+    fn new(opengl: OpenGL) -> App {
+        App {
+            gl: GlGraphics::new(opengl),
+            left_score: 0,
+            left_pos: 1,
+            left_vel: 0,
+            right_score: 0,
+            right_pos: 1,
+            right_vel: 0,
+            ball_x: 0,
+            ball_y: 0,
+            vel_x: 1,
+            vel_y: 1,
+        }
+    }
+
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
@@ -167,19 +183,7 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        left_score: 0,
-        left_pos: 1,
-        left_vel: 0,
-        right_score: 0,
-        right_pos: 1,
-        right_vel: 0,
-        ball_x: 0,
-        ball_y: 0,
-        vel_x: 1,
-        vel_y: 1,
-    };
+    let mut app = App::new(opengl);
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
